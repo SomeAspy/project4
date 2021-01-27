28786014135f48bb8688643a27a67cfc1af69e79
Aiden Baker <33640860+CaptainSnowball@users.noreply.github.com>
2021-01-27 16:33:32 -0500

    dictionary fixes and other stuff

--- game.js
+++ game.js
@@ -1,6 +1,15 @@
+// eslint-disable-next-line no-unused-vars
 function takeGuess(){
     return prompt("Enter a letter");
 }
-function HTML(ID, text){
-    document.getElementById(ID).innerHTML=text;
+
+function rArray(list) {
+    return list[Math.floor(Math.random()*list.length)];
 }
+
+// eslint-disable-next-line no-undef
+let chosen = rArray(nouns);
+console.log(chosen)
+console.log("_ ".repeat(chosen.length))
+console.log(chosen.length)
+document.getElementById("word").innerHTML = "_ ".repeat(chosen.length);
